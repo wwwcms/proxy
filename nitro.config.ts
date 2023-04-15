117881523ff8434c5aa3d6ecebbf333e0ea6d31c
效辉 <dxhuii@qq.com>
2023-04-15 22:39:37 +0800

    修改

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,8 +2,8 @@ import { defineNitroConfig } from 'nitropack'
 
 export default defineNitroConfig({
   routeRules: {
-    '/**': { cors: true, headers: { 'access-control-allow-methods': '*', 'access-control-allow-origin': '*', 'access-control-allow-headers': '*', 'access-control-max-age': '0' } },
-    '/tmdb/**': { swr: 3600, cors: true, headers: { 'access-control-allow-methods': '*', 'access-control-allow-origin': '*', 'access-control-allow-headers': '*', 'access-control-max-age': '0' } },
+    '/**': { cors: false },
+    '/tmdb/**': { swr: 3600, cors: false },
   },
   runtimeConfig: {
     tmdb: {

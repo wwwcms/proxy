27a91e5dbdfad21002d9e4c1c01ca45ff0eae15d
dxhuii <dxhuii@qq.com>
2024-04-17 22:59:18 +0800

    feat: 优化古诗

--- routes/gushi/[...path].ts
+++ routes/gushi/[...path].ts
@@ -19,7 +19,7 @@ export default defineEventHandler(async event => {
   try {
     const { path: id } = event.context.params || {}
     try {
-      const html: any = await $fetch(`https://so.gushiwen.cn/shiwenv_${id}.aspx`)
+      const html: any = await $fetch(`https://so.gushiwen.cn/${id}.aspx`)
       const $ = load(html)
       const sons = $('.contyishang')
       const list = []
@@ -29,10 +29,10 @@ export default defineEventHandler(async event => {
         const title = item.find('h2').text()
         item.find('h2').remove()
         // 移除所有html标签, p标的替换成 \n\n
-        const content = item.html()?.replace(/<br>/g, '\n').replace(/<p>/g, '\n\n').replace(/<\/p>/g, '\n\n').replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split(/\n|\n\n|\n\n\n/).map(item => item.trim()).join('\n')
+        const content = item.html()?.replace(/<br>/g, '\n').replace(/<p>/g, '\n\n').replace(/<\/p>/g, '\n\n').replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split(/\n|\n\n|\n\n\n/).map(item => item.trim()).join('\n').replace(/\n\n\n/g, '\n\n')
         const cankao = $('.cankao')
         cankao.find('p').eq(0).remove()
-        const literature = cankao.html()?.replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split('\n\n\n').map(item => item.replace(/\n/g, '')).join('\n\n')
+        const literature = cankao.html()?.replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split('\n\n\n').map(item => item.replace(/\n/g, '')).join('\n')
         list.push({ title, content, literature })
         // fanyiShow(972,'FFDD9992C473C3C4') 取出中间的 792 和 FFDD9992C473C3C4 的正则
 
@@ -44,10 +44,10 @@ export default defineEventHandler(async event => {
           const conty = $('.contyishang')
           const title = conty.find('h2').text()
           conty.find('h2').remove()
-          const content = $('.contyishang').html()?.replace(/<br>/g, '\n').replace(/<p>/g, '\n').replace(/<\/p>/g, '\n').replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split(/\n|\n\n|\n\n\n/).map(item => item.trim()).join('\n')
+          const content = $('.contyishang').html()?.replace(/<br>/g, '\n').replace(/<p>/g, '\n').replace(/<\/p>/g, '\n').replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split(/\n|\n\n|\n\n\n/).map(item => item.trim()).join('\n').replace(/\n\n\n/g, '\n\n')
           const cankao = $('.cankao')
           cankao.find('p').eq(0).remove()
-          const literature = cankao.html()?.replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split('\n\n\n').map(item => item.replace(/\n/g, '')).join('\n\n')
+          const literature = cankao.html()?.replace(/<[^>]+>/g, '').replace(/▲/g, '').trim().split('\n\n\n').map(item => item.replace(/\n/g, '')).join('\n')
           return { title, content, literature }
         }
 

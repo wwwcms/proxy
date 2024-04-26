e02055d08f67aba6bd0e72dbb866a26f49bcaf7e
dxhuii <dxhuii@qq.com>
2024-04-26 15:47:41 +0800

    feat: 增加集数，少一个请求

--- routes/k9dm/[...path].ts
+++ routes/k9dm/[...path].ts
@@ -8,26 +8,32 @@ export default defineEventHandler(async event => {
     try {
       const browser = await puppeteer.launch({ headless: true })
       const page = await browser.newPage()
-      await page.goto(`https://k9dm.com/index.php/vod/play/id/${id}/sid/${query.sid}/nid/1.html`, { waitUntil: 'networkidle0', timeout: 60000000 })
-      const html = await page.content()
-      const $ = load(html)
-
-      const play_list = $('.fed-play-item.fed-drop-item.fed-visible').find('.fed-part-rows')
       const urls: string[] = []
-      for (let i = 0; i < play_list.length; i++) {
-        const item = $(`${play_list.eq(i)} li`)
-        for (let j = 0; j < item.length; j++) {
-          for (let k = 0; k < item.eq(j).find('a').length; k++) {
-            const url = item.eq(j).find('a').eq(k).attr('href')
-            url?.includes('.html') && urls.push(url!)
+      const nid = Number.parseInt(query.nid as string)
+      if (query.nid) {
+        for (let i = 1; i <= nid; i++)
+          urls.push(`/index.php/vod/play/id/${id}/sid/${query.sid}/nid/${i}.html`)
+      }
+      else {
+        await page.goto(`https://k9dm.com/index.php/vod/play/id/${id}/sid/${query.sid}/nid/1.html`, { waitUntil: 'networkidle0', timeout: 60000000 })
+        const html = await page.content()
+        const $ = load(html)
+
+        const play_list = $('.fed-play-item.fed-drop-item.fed-visible').find('.fed-part-rows')
+        for (let i = 0; i < play_list.length; i++) {
+          const item = $(`${play_list.eq(i)} li`)
+          for (let j = 0; j < item.length; j++) {
+            for (let k = 0; k < item.eq(j).find('a').length; k++) {
+              const url = item.eq(j).find('a').eq(k).attr('href')
+              url?.includes('.html') && urls.push(url!)
+            }
           }
         }
       }
 
       const play: string[] = []
-      console.time('time')
+      console.log(urls, 'urls')
       for await (const url of urls) {
-        console.log(url, 'xxx')
         const u = url.split(/\/|.html/)
         const n = u[u.length - 2]
         await page.goto(`https://k9dm.com${url}`, { waitUntil: 'networkidle0', timeout: 60000000 })
@@ -37,9 +43,6 @@ export default defineEventHandler(async event => {
         const h = src?.split('url=')
         play.push(`第${n}集$${h?.[1]}`)
       }
-
-      console.timeEnd('time')
-      console.log(play, 'urls2')
 
       await page.close()
       await browser.close()

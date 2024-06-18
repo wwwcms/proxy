c1c0422b0735aa58bf40bc712d48e8dcb76bb804
dxhuii <dxhuii@qq.com>
2024-06-18 22:21:01 +0800

    feat: 1

--- routes/m3u8/[...path].ts
+++ routes/m3u8/[...path].ts
@@ -1,4 +1,26 @@
+import https from 'node:https'
+
 export default defineEventHandler(async event => {
+  // const query = getQuery(event)
+  const getUrl = (path: string) => {
+    return new Promise<string>((resolve, reject) => {
+      https.get(path, res => {
+        let data = ''
+
+        res.on('data', chunk => {
+          data += chunk
+        })
+
+        res.on('end', () => {
+          resolve(data)
+        })
+      }).on('error', err => {
+        console.log(`Error: ${err.message}`)
+        reject(err)
+      })
+    })
+  }
+
   const formatData = (play: string) => {
     const ps = play.split('\n')
     const findOneTs = ps.find(item => item.includes('.ts'))
@@ -21,16 +43,53 @@ export default defineEventHandler(async event => {
     return ps.filter(item => item !== 'ziye')
   }
 
+  // async function checkFileExistence(path: string) {
+  //   return new Promise(resolve => {
+  //     fs.access(path, err => {
+  //       if (err)
+  //         resolve(false)
+  //       resolve(true)
+  //     })
+  //   })
+  // }
+
   try {
     const { path: m3u8url } = event.context.params || {}
-    const play: any = await $fetch(decodeURIComponent(m3u8url))
-    if (play) {
+    const http = await getUrl(decodeURIComponent(m3u8url))
+    const data = await getUrl(http)
+    if (data && http.includes('index.m3u8')) {
+      const ids = http.split('index.m3u8')
+      const datas = data.split('\n').filter(item => item.includes('.m3u8'))
+      const url = ids[0] + datas[0]
+      const play = await getUrl(url)
+
       const arr = formatData(play)
-      const m3u8 = arr.filter(item => item).join('\n')
+      const m3u8 = arr.map(item => {
+        if (item.includes('.ts'))
+          return `${ids[0]}${datas[0].split(/mixed.m3u8|index.m3u8/)[0]}${item}`
+        else
+          return item
+      }).filter(item => item).join('\n')
+      // try {
+      //   const { name, nid, sid } = query
+      //   const filePath = `${path.join(process.cwd(), `/ziyuan/${name}/${sid || 1}/${nid}.m3u8`)}`
+      //   // const exist = await checkFileExistence(filePath)
+      //   // if (exist)
+      //   //   return m3u8
+      //   const dirPath = path.dirname(filePath)
+      //   if (!fs.existsSync(dirPath))
+      //     fs.mkdirSync(dirPath, { recursive: true })
+
+      //   fs.writeFileSync(filePath, m3u8, { flag: 'w' })
+      //   console.log('写入成功', filePath)
+      // }
+      // catch (err) {
+      //   console.error(err)
+      // }
       return m3u8
     }
     else {
-      return ''
+      return data
     }
   }
   catch (e: any) {
